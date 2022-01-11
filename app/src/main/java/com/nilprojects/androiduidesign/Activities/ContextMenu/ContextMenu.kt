89e0338dc6453618f4fa27d7086a20575a1ce5e6
Pratham Sonawane <78498799+DADDY-DOUBLESHOT@users.noreply.github.com>
2022-01-11 10:53:07 +0530

    Context Menu output added

--- app/src/main/java/com/nilprojects/androiduidesign/Activities/ContextMenu/ContextMenu.kt
+++ app/src/main/java/com/nilprojects/androiduidesign/Activities/ContextMenu/ContextMenu.kt
@@ -1,11 +1,189 @@
 package com.nilprojects.androiduidesign.Activities.ContextMenu
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.nilprojects.androiduidesign.Activities.ContextMenu.fragments.OutputContextMenu
+import com.nilprojects.androiduidesign.Activities.RubberPicker.Fragments.CodeRubber
+import com.nilprojects.androiduidesign.Activities.RubberPicker.Fragments.OutputRubber
+import com.nilprojects.androiduidesign.Adapter.TabAdapter
+import com.nilprojects.androiduidesign.CommonUtils.Utils
+import com.nilprojects.androiduidesign.R
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment
+import com.yalantis.contextmenu.lib.MenuObject
+import com.yalantis.contextmenu.lib.MenuParams
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
+import kotlinx.android.synthetic.main.toolbar.*
 
 class ContextMenu : AppCompatActivity() {
+
+    private lateinit var contextMenuDialogFragment: ContextMenuDialogFragment
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_context_menu2)
+
+        initToolbar()
+        initMenuFragment()
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.context_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            when (it.itemId) {
+                R.id.context_menu -> {
+                    showContextMenuDialogFragment()
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (::contextMenuDialogFragment.isInitialized && contextMenuDialogFragment.isAdded) {
+            contextMenuDialogFragment.dismiss()
+        } else {
+            finish()
+        }
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setHomeButtonEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(false)
+        }
+
+        toolbar.apply {
+            setNavigationIcon(R.drawable.ic_back)
+            setNavigationOnClickListener { onBackPressed() }
+        }
+
+        tvToolbarTitle.text = "Context Menu"
+    }
+
+    /**
+     * If you want to change the side you need to add 'gravity' parameter,
+     * by default it is MenuGravity.END.
+     *
+     * For example:
+     *
+     * MenuParams(
+     *     actionBarSize = resources.getDimension(R.dimen.tool_bar_height).toInt(),
+     *     menuObjects = getMenuObjects(),
+     *     isClosableOutside = false,
+     *     gravity = MenuGravity.START
+     * )
+     */
+    private fun initMenuFragment() {
+        val menuParams = MenuParams(
+            actionBarSize = resources.getDimension(R.dimen.tool_bar_height).toInt(),
+            menuObjects = getMenuObjects(),
+            isClosableOutside = false
+        )
+
+        contextMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams).apply {
+            menuItemClickListener = { view, position ->
+
+                if (position==6)
+                {
+                    Utils.navigateToGithub("https://github.com/Yalantis/Context-Menu.Android",this@ContextMenu)
+                }
+                else
+                {
+                    Toast.makeText(
+                        this.context,
+                        "Clicked on position: $position",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+            menuItemLongClickListener = { view, position ->
+                Toast.makeText(
+                    this.context,
+                    "Long clicked on position: $position",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    /**
+     * You can use any (drawable, resource, bitmap, color) as image:
+     * menuObject.drawable = ...
+     * menuObject.setResourceValue(...)
+     * menuObject.setBitmapValue(...)
+     * menuObject.setColorValue(...)
+     *
+     * You can set image ScaleType:
+     * menuObject.scaleType = ScaleType.FIT_XY
+     *
+     * You can use any [resource, drawable, color] as background:
+     * menuObject.setBgResourceValue(...)
+     * menuObject.setBgDrawable(...)
+     * menuObject.setBgColorValue(...)
+     *
+     * You can use any (color) as text color:
+     * menuObject.textColor = ...
+     *
+     * You can set any (color) as divider color:
+     * menuObject.dividerColor = ...
+     */
+    private fun getMenuObjects() = mutableListOf<MenuObject>().apply {
+        val close = MenuObject().apply { setResourceValue(R.drawable.icn_close) }
+        val send = MenuObject("Send message").apply { setResourceValue(R.drawable.icn_1) }
+        val like = MenuObject("Like profile").apply {
+            setBitmapValue(BitmapFactory.decodeResource(resources, R.drawable.icn_2))
+        }
+        val addFriend = MenuObject("Add to friends").apply {
+            drawable = BitmapDrawable(
+                resources,
+                BitmapFactory.decodeResource(resources, R.drawable.icn_3)
+            )
+        }
+        val addFavorite = MenuObject("Add to favorites").apply {
+            setResourceValue(R.drawable.icn_4)
+        }
+        val block = MenuObject("Block user").apply { setResourceValue(R.drawable.icn_5) }
+        val code = MenuObject("Github Code").apply { setResourceValue(R.drawable.github_circle_dark) }
+
+        add(close)
+        add(send)
+        add(like)
+        add(addFriend)
+        add(addFavorite)
+        add(block)
+        add(code)
+    }
+
+    private fun showContextMenuDialogFragment() {
+        if (supportFragmentManager.findFragmentByTag(ContextMenuDialogFragment.TAG) == null) {
+            contextMenuDialogFragment.show(supportFragmentManager, ContextMenuDialogFragment.TAG)
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/nilprojects/androiduidesign/Activities/ContextMenu/fragments/OutputContextMenu.kt
+++ app/src/main/java/com/nilprojects/androiduidesign/Activities/ContextMenu/fragments/OutputContextMenu.kt
@@ -1,60 +1,48 @@
 package com.nilprojects.androiduidesign.Activities.ContextMenu.fragments
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import com.nilprojects.androiduidesign.R
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment
+import com.yalantis.contextmenu.lib.MenuObject
+import com.yalantis.contextmenu.lib.MenuParams
+import kotlinx.android.synthetic.main.toolbar.*
+import android.view.Menu
+import android.view.MenuItem
+import android.R.menu
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.view.MenuInflater
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OutputContextMenu.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
+
+
 class OutputContextMenu : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var contextMenuDialogFragment: ContextMenuDialogFragment
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_output_context_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_output_context_menu, container, false)
+
+//        initToolbar()
+//        initMenuFragment()
+//        setHasOptionsMenu(true);
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OutputContextMenu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OutputContextMenu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
+
+
+
 }
